98340e1fb1d5f6ea343f03a979c9dc63714b9e7b
Lê Thành Thảo <79634248+lethao323@users.noreply.github.com>
2026-07-14 05:32:48 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,6 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
-
-
 `default_nettype none
 
-// Module MUX 2:1 cơ bản
+// Module MUX 2:1 cơ sở
 module mux2to1 (
     input wire i0,
     input wire i1,
@@ -15,7 +10,7 @@ module mux2to1 (
     assign y = sel ? i1 : i0;
 endmodule
 
-// Top Module
+// Top Module (Giữ nguyên tên này để khớp với info.yaml)
 module tt_um_thao_4to1_mux (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
@@ -26,13 +21,13 @@ module tt_um_thao_4to1_mux (
     input  wire       clk,
     input  wire       rst_n
 );
-    // Logic 4:1 MUX
+    // Logic kết nối
     wire y01, y23;
     mux2to1 m1 (.i0(ui_in[0]), .i1(ui_in[1]), .sel(ui_in[4]), .y(y01));
     mux2to1 m2 (.i0(ui_in[2]), .i1(ui_in[3]), .sel(ui_in[4]), .y(y23));
     mux2to1 m3 (.i0(y01), .i1(y23), .sel(ui_in[5]), .y(uo_out[0]));
 
-    // Gán các chân không dùng về 0
+    // Gán các chân không dùng
     assign uo_out[7:1] = 7'b0;
     assign uio_out = 8'b0;
     assign uio_oe  = 8'b0;
